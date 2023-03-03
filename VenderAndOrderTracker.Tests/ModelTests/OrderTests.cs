5952370df376b61334ee2d12f9e1dcdd5eec2f6d
Noah Atkinson <noahatkinson1.1@gmail.com>
2023-03-03 09:04:58 -0800

    pass return and set description

--- VenderAndOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VenderAndOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -15,5 +15,29 @@ namespace VenderAndOrderTracker.Tests
       Order newOrder = new Order("test");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      string description = "10 Breads";
+
+      Order newOrder = new Order(description);
+      string result = newOrder.Description;
+
+      Assert.AreEqual(description, result);
+    }
+
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      string description = "10 Breads";
+      Order newOrder = new Order(description);
+
+      string updatedDescription = "20 Pastries";
+      newOrder.Description = updatedDescription;
+      string result = newOrder.Description;
+
+      Assert.AreEqual(updatedDescription, result);
+    }
   }
 }
